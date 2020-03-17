c38b86da186dcb8e0c85975b32708e427a5b2b74
Your Name <your.email@your-place.com>
2020-03-17 18:35:13 -0400

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App';
 
+Enzyme.configure({ adapter: new EnzymeAdapter()});
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
 });
